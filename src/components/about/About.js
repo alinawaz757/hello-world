5d49a3933cb628a685d070a67dee785b19086dc9
chand757 <chand757.dev@gmail.com>
2022-12-06 01:43:04 +0500

    contact page completed

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <h1>About</h1>
+      <h3>version 1.0.0</h3>
+    </>
+  );
 };
 
 export default About;
